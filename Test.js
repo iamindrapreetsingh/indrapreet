9866ebae759c8f93f226fe62b7dcd865079270cf
iamindrapreetsingh <51911982+iamindrapreetsingh@users.noreply.github.com>
2019-07-01 10:46:43 +0530

    Update Test.js

--- Test.js
+++ Test.js
@@ -4,17 +4,40 @@ let day;
 let hours;
 function test()
 {
-    setInterval(change,3000);
+    setTimeout(change,3000);
+
 }
 
 function change()
 {
     hour=d.getHours();
+    minute=d.getMinutes();
 
-    if(hour>=10 && hour<=15)
+    if(hour>=09 && hour<=15)
     document.getElementById("dp").src="myself3.jpg";
 
     if(hour>=16 && hour<=21)
-    document.getElementById("dp").src="myself2.jpg";
+    document.getElementById("dp").src="myself.jpg";
 
+}
+
+let content;
+
+function demo()
+{
+  var oReq = new XMLHttpRequest();
+  oReq.onload=perform;
+  oReq.open("GET", "https://api.github.com/users/iamindrapreetsingh/repos");
+  oReq.send();
+}
+
+function perform()
+{
+   var data=JSON.parse(this.responseText);
+   content=document.getElementById("repo");
+
+   for(let i=0;i<data.length;i++)
+   content.innerHTML=content.innerHTML+"<br>/"+data[i]["name"];
+   
+   
 }
